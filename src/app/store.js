817715606b7292a9581de69eac4902e4bdfc251d
Chathuranga Niroshana <134888303+Chathuranga-Niroshana@users.noreply.github.com>
2025-07-17 21:56:31 +0530

    post slice created

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from '../features/auth/authSlice'
 import usersReducer from '../features/users/usersSlice'
+import postReducer from '../features/posts/postSlice'
 
 export const store = configureStore({
     reducer: {
         auth: authReducer,
-        users: usersReducer
+        users: usersReducer,
+        posts: postReducer
     }
 })
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import PostCard from '../../components/widgets/PostCard';
 import MostEngagingUsers from './MostEngagingUsers'
-import { samplePosts } from '../../../database/samplePosts';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchPost } from '../../features/posts/postSlice';
 
 
 const Home = () => {
+    const posts = useSelector((state) => state.posts.allPosts);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        fetchPost()(dispatch).then((result) => {
+            if (fetchPost.fulfilled.match(result)) {
+                console.log('Posts fetched successfully:', result.payload);
+            } else {
+                console.error('Failed to fetch posts:', result.error.message);
+            }
+        }).catch((error) => {
+            console.error('Error fetching posts:', error);
+        });
+    }, [dispatch])
     return (
         <div className=''>
             {/* today most engaging 5 users */}
@@ -14,7 +29,7 @@ const Home = () => {
 
             {/* posts */}
             <div className='flex flex-col justify-center items-center gap-10 mt-10'>
-                {samplePosts.map((post) => (
+                {posts?.map((post) => (
                     <PostCard key={post.id} post={post} />
                 ))}
             </div>
